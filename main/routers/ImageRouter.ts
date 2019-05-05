5bf8fd81cf330ed8119ecace425f818eda8b335e
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-05-05 09:09:04 -0500

    On branch master
    Se corrigen bugs
    
    Changes to be committed:
    	modified:   main/routers/ImageRouter.ts

--- main/routers/ImageRouter.ts
+++ main/routers/ImageRouter.ts
@@ -1,11 +1,12 @@
 import express, { Application } from "express";
+import path from "path";
 
 export default class ImageRouter {
   private endPoint: string;
 
   constructor(app: Application, version: string) {
     this.endPoint = `/static`;
-
-    app.use(this.endPoint, express.static("/app/dist/contrib/assets"));
+    
+    app.use(this.endPoint, express.static(path.join(__dirname, "../contrib/assets")));
   }
 }
