c8816d80060d0c0c50e769a8dca23d977259b773
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-05-05 20:23:15 -0500

    On branch master
    Se corrige bug
    
    Changes to be committed:
    	modified:   main/controllers/HotelController.ts

--- main/controllers/HotelController.ts
+++ main/controllers/HotelController.ts
@@ -5,9 +5,12 @@ import HotelBusiness from "@app/contrib/business/HotelBusiness";
 export default class HotelController {
 
   public list(req: Request, res: Response): void {
-    const amenities = req.query.amenities ? req.query.amenities.split(",") : null;
+    const page: number | null = req.query.page ? Number(req.query.page) : null;
+    const name: string | null = req.query.name ? req.query.name : null;
+    const amenities: Array<string> | null = req.query.amenities ? req.query.amenities.split(",") : null;
+    const stars: number | null = req.query.stars ? Number(req.query.stars) : null;
     res.json(
-      new HotelQueryFilter(req.query.page, req.query.name, amenities, req.query.stars)
+      new HotelQueryFilter(page, name, amenities, stars)
         .resolve());
   }
 
